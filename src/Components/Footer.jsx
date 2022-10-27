41818bca444bcb1740ee14a0912b07b958d0a220
bitsUnxte <f20210274@goa.bits-pilani.ac.in>
2022-10-27 14:57:33 +0530

    footer_padding_added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link, Box, Text } from '@chakra-ui/react';
 
 const Footer = () => {
   return (
-    <Box bg='#183249'>
+    <Box bg='#183249' p='1.4em'>
       <Text color='#EFF8FA'>
         Made with ❤️ by{' '}
         <Link href='https://devsoc.club' color={'blue'}>
